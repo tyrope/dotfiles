4719260a778fa45a0fa6928d825e88cb539ecaed
Dimitri Molenaars <Tyrope@TyRope.nl>
2014-05-13 22:25:06 +0200

    Fix the future import thing.

--- autoCommitter.py
+++ autoCommitter.py
@@ -7,6 +7,7 @@ This work is free. You can redistribute it and/or modify it under the
 terms of the Do What The Fuck You Want To Public License, Version 2,
 as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 """
+from __future__ import print_function
 
 conf = {}
 #----------------------
@@ -26,7 +27,6 @@ conf['branch'] = 'master' # Remote branch you want to push to.
 #- End of Configuration -
 #------------------------
 
-from __future__ import print_function
 from time import sleep
 from datetime import datetime
 import subprocess
